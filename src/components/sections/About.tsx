7c85e40f07dff81ea463871e074bf0ac33ea46bf
glaygaming <glaygaming57@gmail.com>
2026-06-26 13:05:08 +0800

    Update portfolio

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 
 export default function About() {
   const images = [
-    "/images/gradpic.jpg",
-    "/images/about.jpg",
+    "/images/gradpic.JPG",
+    "/images/about.JPG",
     "/images/engineer.jpg",
   ];
 
