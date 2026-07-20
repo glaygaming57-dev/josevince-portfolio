e685e2c8a0fb1bc62cf09767e8e1dc3f2f3916cb
glaygaming <glaygaming57@gmail.com>
2026-07-20 16:57:51 +0800

    cv

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -165,7 +165,7 @@ hover:-translate-y-1
 
   {/* CV */}
   <a
-  href="https://drive.google.com/file/d/1LMlA6cSkpwlzDj6FRbJvziuTTwb9dePS/view?usp=sharing"
+  href="https://drive.google.com/file/d/1F_iU2rxyWmnA4sE8VlmMKKVaxvu1o2Ve/view?usp=sharing"
   target="_blank"
   rel="noopener noreferrer"
   className="
